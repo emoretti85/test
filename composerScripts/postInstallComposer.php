683d11a4bcd42c84a185e7660b9c9938d73e466d
Ettore Moretti <ettoremoretti27@gmail.com>
2020-02-12 13:34:35 +0100

    t

--- composerScripts/postInstallComposer.php
+++ composerScripts/postInstallComposer.php
@@ -1,21 +1,28 @@
 #!/usr/bin/php
 <?php
+namespace em\composerScripts;
+
 define ("DS", DIRECTORY_SEPARATOR);
 
-//Recupero il path del progetto
-$composerProjectBase = getcwd().DS;
 
-//Recupero il path del package
-$bootstrapSkeletonFolder = dirname(dirname(__FILE__)). DS . "skel";
+class InstallerScripts{
+    public static function postInstall(){
+        //Recupero il path del progetto
+        $composerProjectBase = getcwd().DS;
 
-//Ciclo nella folder Skel dove sono le cartelle e i file da creare nel progetto
-foreach (glob($bootstrapSkeletonFolder . '/*' , GLOB_ONLYDIR) as $dir) {
-    //Creo la cartella nel progetto
-    mkdir($composerProjectBase.basename($dir));
-    
-    //Copio tutti i file
-    $files = array_diff(scandir($dir), array('.', '..'));
-    foreach ($files as $file) {
-        copy($dir.DS.$file, $composerProjectBase.basename($dir).DS.$file);
+        //Recupero il path del package
+        $bootstrapSkeletonFolder = dirname(dirname(__FILE__)). DS . "skel";
+
+        //Ciclo nella folder Skel dove sono le cartelle e i file da creare nel progetto
+        foreach (glob($bootstrapSkeletonFolder . '/*' , GLOB_ONLYDIR) as $dir) {
+            //Creo la cartella nel progetto
+            mkdir($composerProjectBase.basename($dir));
+            
+            //Copio tutti i file
+            $files = array_diff(scandir($dir), array('.', '..'));
+            foreach ($files as $file) {
+                copy($dir.DS.$file, $composerProjectBase.basename($dir).DS.$file);
+            }
+        }
     }
 }
